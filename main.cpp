ad43178e63987ee8940416e52292d9120f55ef15
ewoifuoi <arum1kuy1@outlook.com>
2026-04-07 13:45:40 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <vulkan/vulkan_raii.hpp>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+
+    }
+
+    void cleanup() {
+
+    }
+};
+
+int main()
+{
+    try
+    {
+        HelloTriangleApplication app;
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
