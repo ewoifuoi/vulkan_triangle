25a3aafb1f1849022b8ec2c27f6aec69b9d5f52d
ewoifuoi <arum1kuy1@outlook.com>
2026-04-12 20:22:54 +0800

    add graphics pipeline

--- main.cpp
+++ main.cpp
@@ -530,6 +530,26 @@ private:
     void createGraphicsPipeline() {
         auto vertShaderCode = readFile("../shaders/shader.vert.spv");
         auto fragShaderCode = readFile("../shaders/shader.frag.spv");
+
+        VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+        VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+        VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+        vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+        vertShaderStageInfo.module = vertShaderModule;
+        vertShaderStageInfo.pName = "main";
+
+        VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+        fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        fragShaderStageInfo.module = fragShaderModule;
+        fragShaderStageInfo.pName = "main";
+
+        VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+        vkDestroyShaderModule(device, fragShaderModule, nullptr);
+        vkDestroyShaderModule(device, vertShaderModule, nullptr);
     }
 
     static std::vector<char> readFile(const std::string& filename) {
@@ -543,6 +563,18 @@ private:
         file.read(buffer.data(), fileSize);
         file.close();
         return buffer;
+    }
+
+    VkShaderModule createShaderModule(const std::vector<char>& code) {
+        VkShaderModuleCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+        VkShaderModule shaderModule;
+        if(VK_SUCCESS != vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule)) {
+            throw std::runtime_error("failed to create shader module!");
+        }
+        return shaderModule;
     }
 
     void cleanup() {
@@ -559,6 +591,8 @@ private:
         vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
+
+        
     }
 };
 
